4487244b586515db8d62b2bd365e7ec479540841
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-11 16:14:14 -0500

    feat: remove image banner

--- src/layouts/presentation/Presentation.tsx
+++ src/layouts/presentation/Presentation.tsx
@@ -4,12 +4,10 @@ import "./styles/Presentation.css";
 function Presentation() {
   return (
     <>
-      <div className="presentation-content">
-        <h1 className="presentation">
-          Full-stack
-          <span> developer</span>
-        </h1>
-      </div>
+      <h1 className="presentation">
+        Full-stack
+        <span> developer</span>
+      </h1>
       <Hello />
       <Profile />
     </>

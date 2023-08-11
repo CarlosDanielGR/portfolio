d628c83afc16400c84c022c70178938b25900fc4
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-11 11:58:30 -0500

    feat: navbar partially completed

--- src/layouts/navbar/Navbar.tsx
+++ src/layouts/navbar/Navbar.tsx
@@ -1,11 +1,33 @@
-import "./Navbar.css";
+import { Link } from ".";
+import { INavLink } from "./interfaces/link.interface";
+import "./styles/Navbar.css";
 
 function Navbar() {
+  const LINKS: INavLink[] = [
+    {
+      id: 1,
+      name: "Projects",
+      route: "#projects",
+    },
+    {
+      id: 2,
+      name: "Employment",
+      route: "#employment",
+    },
+    {
+      id: 3,
+      name: "Skills",
+      route: "#skills",
+    },
+  ];
+
   return (
     <ul className="navbar">
-      <li>Projects</li>
-      <li>Employment</li>
-      <li>Skills</li>
+      {LINKS.filter((data) => !data.hide).map((links) => (
+        <li key={links.id}>
+          <Link link={links} />
+        </li>
+      ))}
     </ul>
   );
 }
--- src/layouts/navbar/index.ts
+++ src/layouts/navbar/index.ts
@@ -1,1 +1,2 @@
 export { default as Navbar } from "./Navbar";
+export { default as Link } from "./components/Link";

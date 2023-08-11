3aa1ba5cf434b831dfed5c6392b2a096ff2b044f
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-11 11:28:23 -0500

    feat: first change in navbar component

---
+++ src/layouts/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <ul className="navbar">
+      <li>Projects</li>
+      <li>Employment</li>
+      <li>Skills</li>
+    </ul>
+  );
+}
+export default Navbar;

42a8ead68afa874642290c2c7838d35dd982d01f
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-11 13:06:20 -0500

    feat: change app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Navbar } from "./layouts/navbar";
+import { Presentation } from "./layouts/presentation";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Presentation />
     </>
   );
 }
--- src/layouts/presentation/Presentation.tsx
+++ src/layouts/presentation/Presentation.tsx
@@ -1,11 +1,15 @@
+import { Hello } from ".";
 import "./styles/Presentation.css";
 
 function Presentation() {
   return (
-    <h1 className="presentation">
-      <span>Full-stack </span>
-      developer
-    </h1>
+    <>
+      <h1 className="presentation">
+        <span>Full-stack </span>
+        developer
+      </h1>
+      <Hello />
+    </>
   );
 }
 export default Presentation;

702b3050f3312938b90327d9458829cea1eb56e5
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-11 12:49:32 -0500

    feat: title main created

---
+++ src/layouts/presentation/Presentation.tsx
@@ -0,0 +1,11 @@
+import "./styles/Presentation.css";
+
+function Presentation() {
+  return (
+    <h1 className="presentation">
+      <span>Full-stack </span>
+      developer
+    </h1>
+  );
+}
+export default Presentation;

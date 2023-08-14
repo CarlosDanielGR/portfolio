0cb4a13c26e36ddc0b53aef55b3564a72ab8e6eb
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-14 16:18:29 -0500

    feat: restore navbar

--- src/layouts/navbar/Navbar.tsx
+++ src/layouts/navbar/Navbar.tsx
@@ -1,4 +1,34 @@
+import { Link } from ".";
+import { INavLink } from "./interfaces/link.interface";
+import "./styles/Navbar.css";
+
 function Navbar() {
-  return <></>;
+  const LINKS: INavLink[] = [
+    {
+      id: 1,
+      name: "Projects",
+      route: "#projects",
+    },
+    {
+      id: 2,
+      name: "Employment",
+      route: "#employment",
+    },
+    {
+      id: 3,
+      name: "Skills",
+      route: "#skills",
+    },
+  ];
+
+  return (
+    <ul className="navbar">
+      {LINKS.filter((data) => !data.hide).map((links) => (
+        <li key={links.id}>
+          <Link link={links} />
+        </li>
+      ))}
+    </ul>
+  );
 }
 export default Navbar;

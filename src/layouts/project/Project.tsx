af647dba7d8282dc041e47bf0d5b749422a7808c
Carlos Daniel Guzman Rios <90700332+CarlosDanielGR@users.noreply.github.com>
2023-08-15 16:16:06 -0500

    Project section completed (#5)
    * feat: layout project completed
    
    * feat: first config project completed
    
    * styles: responsive project completed

--- src/layouts/project/Project.tsx
+++ src/layouts/project/Project.tsx
@@ -1,4 +1,16 @@
+import { ProjectList } from ".";
+
 function Project() {
-  return <div>Project</div>;
+  return (
+    <div id="project">
+      <h4>Projects</h4>
+      <p>
+        As a developer, I have successfully contributed to various projects. In
+        some of them, I even initiated the project from its structure
+        (archetype). Here is the list of the projects I have worked on.
+      </p>
+      <ProjectList />
+    </div>
+  );
 }
 export default Project;

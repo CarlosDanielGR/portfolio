24d8e84c479ba66d68c4e98e0eb7823773eb3a6c
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-16 14:06:18 -0500

    styles: timeline mobile version completed

--- src/layouts/project/components/Timeline.tsx
+++ src/layouts/project/components/Timeline.tsx
@@ -3,12 +3,46 @@ import "./../styles/Timeline.css";
 function Timeline() {
   return (
     <div className="timeline">
-      <div>
+      <div className="timeline-section">
         <div className="timeline-middle">
           <div className="timeline-circle"></div>
         </div>
+        <div className="timeline-content">
+          <span>2021 - rick-and-morty</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            eum ipsa soluta eligendi rerum fugiat enim similique repellat,
+            doloremque fugit officia magni sunt accusamus quo autem cupiditate
+            delectus sed aliquid.
+          </p>
+        </div>
+      </div>
+      <div className="timeline-section">
         <div className="timeline-middle">
           <div className="timeline-circle"></div>
+        </div>
+        <div className="timeline-content">
+          <span>2021 - rick-and-morty</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            eum ipsa soluta eligendi rerum fugiat enim similique repellat,
+            doloremque fugit officia magni sunt accusamus quo autem cupiditate
+            delectus sed aliquid.
+          </p>
+        </div>
+      </div>
+      <div className="timeline-section">
+        <div className="timeline-middle">
+          <div className="timeline-circle"></div>
+        </div>
+        <div className="timeline-content">
+          <span>2021 - rick-and-morty</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            eum ipsa soluta eligendi rerum fugiat enim similique repellat,
+            doloremque fugit officia magni sunt accusamus quo autem cupiditate
+            delectus sed aliquid.
+          </p>
         </div>
       </div>
     </div>

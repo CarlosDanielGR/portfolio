2a321886827d002ab0c0f6cd48862d0e4d93db7b
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-16 14:41:54 -0500

    styles: design timeline completed

--- src/layouts/project/components/Timeline.tsx
+++ src/layouts/project/components/Timeline.tsx
@@ -45,6 +45,20 @@ function Timeline() {
           </p>
         </div>
       </div>
+      <div className="timeline-section">
+        <div className="timeline-middle">
+          <div className="timeline-circle"></div>
+        </div>
+        <div className="timeline-content">
+          <span>2021 - rick-and-morty</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            eum ipsa soluta eligendi rerum fugiat enim similique repellat,
+            doloremque fugit officia magni sunt accusamus quo autem cupiditate
+            delectus sed aliquid.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }

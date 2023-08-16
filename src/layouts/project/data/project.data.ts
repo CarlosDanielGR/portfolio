5e51a0cef9b616b02c69080bbcbcbeb8ca644012
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-16 17:09:17 -0500

    feat: partially changes in projects data

--- src/layouts/project/data/project.data.ts
+++ src/layouts/project/data/project.data.ts
@@ -7,4 +7,83 @@ export const PROJECTS: Project[] = [
       "Simple page created for informative purposes, during my development stage the use of native Angular animations stands out.",
     url: "https://inlacom.com/es/",
   },
+  {
+    title: "2021 - Partner",
+    content:
+      "Main platform has an administration module; I was the creator of the project and the Ionic framework was added to run the mobile version, the application has service worker and PWA.",
+    url: "https://partner.inlacom.com/es/login",
+  },
+  {
+    title: "2022 - Inlazepay",
+    content:
+      "The application was created based on what had been done in the already mentioned Landing project, in my development stage only support was provided.",
+    url: "https://inlazepay.com/es/",
+  },
+  {
+    title: "2023 - Inlabet",
+    content:
+      "Project created from scratch, the NGRX state handler was applied for the first time in the company, I was the only developer from its creation to its first deployment to production.",
+    url: "https://inlabet.com/co/bookmakers",
+  },
+  {
+    title: "2023 - Inlajobs",
+    content:
+      "Platform for job vacancies, was created from scratch; it implements typical Angular reactive form, I was the only developer during its creation stage until deployment to production.",
+    url: "https://inlajobs.com/",
+  },
+  {
+    title: "",
+    content: "",
+    url: "",
+  },
+  {
+    title: "",
+    content: "",
+    url: "",
+  },
+  {
+    title: "",
+    content: "",
+    url: "",
+  },
+  {
+    title: "",
+    content: "",
+    url: "",
+  },
+  {
+    title: "",
+    content: "",
+    url: "",
+  },
+  {
+    title: "",
+    content: "",
+    url: "",
+  },
+  {
+    title: "",
+    content: "",
+    url: "",
+  },
+  {
+    title: "",
+    content: "",
+    url: "",
+  },
+  {
+    title: "",
+    content: "",
+    url: "",
+  },
+  {
+    title: "",
+    content: "",
+    url: "",
+  },
+  {
+    title: "",
+    content: "",
+    url: "",
+  },
 ];

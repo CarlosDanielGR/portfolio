741406fb7a09a96a1938da1efdf58e8033d5a118
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-17 12:37:42 -0500

    feat: last projects added

--- src/layouts/project/data/project.data.ts
+++ src/layouts/project/data/project.data.ts
@@ -14,10 +14,28 @@ export const PROJECTS: Project[] = [
     url: "https://partner.inlacom.com/es/login",
   },
   {
+    title: "2022 - Learn-nest",
+    content:
+      "Project created for learning Nestjs, it has different branches in which there are connections to Mongodb and Postgres.",
+    url: "https://github.com/CarlosDanielGR/learn-nest",
+  },
+  {
     title: "2022 - Inlazepay",
     content:
       "The application was created based on what had been done in the already mentioned Landing project, in my development stage only support was provided.",
     url: "https://inlazepay.com/es/",
+  },
+  {
+    title: "2023 - Angular-Ngrx",
+    content:
+      "Project created for learning and using the Angular NGRX state handler.",
+    url: "https://github.com/CarlosDanielGR/Angular-ngrx",
+  },
+  {
+    title: "2023 - Archetype",
+    content:
+      "Project created as Angular project structures from scratch, the project has different branches based on different needs.",
+    url: "https://github.com/CarlosDanielGR/archetype",
   },
   {
     title: "2023 - Inlabet",
@@ -26,64 +44,15 @@ export const PROJECTS: Project[] = [
     url: "https://inlabet.com/co/bookmakers",
   },
   {
+    title: "2023 - Learn-react",
+    content:
+      "The project is based on the 'Docker' documentation, it is used for learning react and its dependencies.",
+    url: "https://github.com/CarlosDanielGR/learn-react",
+  },
+  {
     title: "2023 - Inlajobs",
     content:
       "Platform for job vacancies, was created from scratch; it implements typical Angular reactive form, I was the only developer during its creation stage until deployment to production.",
     url: "https://inlajobs.com/",
-  },
-  {
-    title: "",
-    content: "",
-    url: "",
-  },
-  {
-    title: "",
-    content: "",
-    url: "",
-  },
-  {
-    title: "",
-    content: "",
-    url: "",
-  },
-  {
-    title: "",
-    content: "",
-    url: "",
-  },
-  {
-    title: "",
-    content: "",
-    url: "",
-  },
-  {
-    title: "",
-    content: "",
-    url: "",
-  },
-  {
-    title: "",
-    content: "",
-    url: "",
-  },
-  {
-    title: "",
-    content: "",
-    url: "",
-  },
-  {
-    title: "",
-    content: "",
-    url: "",
-  },
-  {
-    title: "",
-    content: "",
-    url: "",
-  },
-  {
-    title: "",
-    content: "",
-    url: "",
   },
 ];

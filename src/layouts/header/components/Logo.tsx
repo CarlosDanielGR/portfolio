af453b2b21d278b520764cdb5cd4db1972fc3972
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-10-14 23:51:05 -0500

    onInit function added

--- src/layouts/header/components/Logo.tsx
+++ src/layouts/header/components/Logo.tsx
@@ -1,8 +1,16 @@
 import "./../styles/Logo.css";
 
 function Logo() {
+  const onInit = () => () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div className="logo">
+    <div className="logo" onClick={onInit()}>
       <figure className="logo-img">
         <img src="./public/images/logo.png" alt="logo" />
       </figure>

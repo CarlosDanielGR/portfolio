a84374c286ce9420be4d7f96af28faf60f2da503
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-10-15 22:59:27 -0500

    Style skills changed

--- src/layouts/skills/components/SkillElement.tsx
+++ src/layouts/skills/components/SkillElement.tsx
@@ -9,8 +9,10 @@ function SkillElement({ skill }: Props) {
   const levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <div className="skill">
-      <img src={skill.src} alt={skill.alt} />
-      <span>{skill.name}</span>
+      <div className="skill-name">
+        <img src={skill.src} alt={skill.alt} />
+        <span>{skill.name}</span>
+      </div>
       <div className="skill-content-levels">
         {levels.map((value: number) => (
           <div

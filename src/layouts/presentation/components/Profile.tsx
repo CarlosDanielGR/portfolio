3e73204d674c22b5e53d2a5e057502795b78ac4f
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-12-19 20:44:23 -0500

    fix profile text

--- src/layouts/presentation/components/Profile.tsx
+++ src/layouts/presentation/components/Profile.tsx
@@ -1,27 +1,12 @@
-import { Button } from "@mui/material";
 import "./../styles/Profile.css";
 
 function Profile() {
   return (
-    <div className="profile">
-      <h3>
-        Passionate developer committed to continuous learning. I enjoy facing
-        challenges and seeking optimal solutions for complex problems, always
-        following the best development practices.
-      </h3>
-      <div className="profile-buttons">
-        <Button
-          variant="outlined"
-          href="https://wa.link/er6wkp"
-          target="_blank"
-        >
-          Contact us
-        </Button>
-        <Button variant="contained" href="./fullstack_carlosguzman.pdf" download="cv_carlosguzman">
-          Download CV
-        </Button>
-      </div>
-    </div>
+    <p className="profile">
+      I am a full-stack developer, specializing in frontend and semi-senior in
+      Angular. I also have experience in backend development using NestJS and
+      Laravel.
+    </p>
   );
 }
 export default Profile;

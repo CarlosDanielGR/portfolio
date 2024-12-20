ee94447e3ec3dfabff6b3ff0f6442baf8059c698
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-12-19 21:51:48 -0500

    fix timeline employment completed

--- src/layouts/employment/Employment.tsx
+++ src/layouts/employment/Employment.tsx
@@ -34,48 +34,6 @@ function Employment() {
       <h5 className="subtitle">Employment</h5>
       <Job
         role="Frontend developer"
-        company="Betenlace SAS"
-        date="October 2021 - July 2023"
-      >
-        <p>
-          As responsible for the integral support of the company's projects, I
-          carried out the development of new projects from their conception to
-          their final implementation. My focus was on applying the best
-          development and analysis practices, ensuring optimal performance and
-          proper operation of the applications.{" "}
-        </p>
-        <ul>
-          <li>
-            <strong>Change and standards management</strong>: I supervised and
-            managed changes in projects, ensuring code quality and adapting
-            structures to specific requirements. I defined code standards and
-            established clear guidelines to ensure consistency and efficiency in
-            development.
-          </li>
-          <li>
-            <strong>Technical support and collaboration</strong>: I provided
-            direct support to front-end developers, resolving technical doubts,
-            offering guidance in the implementation of specific features,
-            addressing conflicts in Git and collaborating in debugging bugs. I
-            actively participated in the final deployment process of the
-            projects.
-          </li>
-          <li>
-            <strong>Technical interviews</strong>: I conducted technical
-            interviews to select new front-end developers, evaluating their
-            skills and ensuring that they matched the team's needs and company
-            standards.
-          </li>
-        </ul>
-        <p>
-          My main objective was to facilitate a fluid and collaborative work
-          environment, promoting the professional growth of the team and
-          ensuring the success of each project.
-        </p>
-        <Tags tags={tagsBetenlace} />
-      </Job>
-      <Job
-        role="Frontend developer"
         company="Innovating Solutions"
         date="September 2023 - September 2024"
       >
@@ -116,6 +74,48 @@ function Employment() {
           contributing to the collective success of the team.{" "}
         </p>
         <Tags tags={tagsInnovating} />
+      </Job>
+      <Job
+        role="Frontend developer"
+        company="Betenlace SAS"
+        date="October 2021 - July 2023"
+      >
+        <p>
+          As responsible for the integral support of the company's projects, I
+          carried out the development of new projects from their conception to
+          their final implementation. My focus was on applying the best
+          development and analysis practices, ensuring optimal performance and
+          proper operation of the applications.{" "}
+        </p>
+        <ul>
+          <li>
+            <strong>Change and standards management</strong>: I supervised and
+            managed changes in projects, ensuring code quality and adapting
+            structures to specific requirements. I defined code standards and
+            established clear guidelines to ensure consistency and efficiency in
+            development.
+          </li>
+          <li>
+            <strong>Technical support and collaboration</strong>: I provided
+            direct support to front-end developers, resolving technical doubts,
+            offering guidance in the implementation of specific features,
+            addressing conflicts in Git and collaborating in debugging bugs. I
+            actively participated in the final deployment process of the
+            projects.
+          </li>
+          <li>
+            <strong>Technical interviews</strong>: I conducted technical
+            interviews to select new front-end developers, evaluating their
+            skills and ensuring that they matched the team's needs and company
+            standards.
+          </li>
+        </ul>
+        <p>
+          My main objective was to facilitate a fluid and collaborative work
+          environment, promoting the professional growth of the team and
+          ensuring the success of each project.
+        </p>
+        <Tags tags={tagsBetenlace} />
       </Job>
     </div>
   );
